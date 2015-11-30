09d311ddeca2e49db6d14173f4699d911efe6086
Adlai Holler <guy.on.internet@icloud.com>
2015-11-29 19:38:28 -0800

    Create and implement a weatherProvider interface, use it to hook up to another weather source

--- main.go
+++ main.go
@@ -5,48 +5,119 @@ import (
 	"encoding/json"
 	"strings"
 	"errors"
+	"time"
+	"log"
 )
 
 func main() {
+	mw := multiWeatherProvider{
+		openWeatherMap{apiKey: "your-key-here"},
+		weatherUnderground{apiKey: "your-key-here"},
+	}
 	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		begin := time.Now()
 		city := strings.SplitN(r.URL.Path, "/", 3)[2]
 
-		data, err := query(city)
+		temp, err := mw.temperature(city)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		json.NewEncoder(w).Encode(data)
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"city": city,
+			"temp": temp,
+			"took": time.Since(begin).String(),
+		})
 	})
 	http.ListenAndServe(":8080", nil)
 }
 
-type weatherData struct {
-	Name string `json:"name"`
-	Main struct {
-		Kelvin float64 `json:"temp"`
-	} `json:"main"`
+// Create interface (like protocol) that requires a temperature function
+type weatherProvider interface {
+	temperature(city string) (float64, error) // in Kelvin
 }
 
-func query(city string) (weatherData, error) {
-	apiKey := ""
-	if apiKey == "" {
-		return weatherData{}, errors.New("No API key. Open main.go and set an API key.")
+// Create func to query multiple weather providers and return the average temp.
+type multiWeatherProvider []weatherProvider
+
+func (w multiWeatherProvider) temperature(city string) (float64, error) {
+	sum := 0.0
+
+	for _, provider := range w {
+		k, err := provider.temperature(city)
+		if err != nil {
+			return 0, err
+		}
+
+		sum += k
 	}
-	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + apiKey + "&q=" + city)
+
+	return sum / float64(len(w)), nil
+}
+
+// 
+type multiWeatherProviders []weatherProvider
+
+// Create OpenWeatherMap-backed weatherProvider implementation
+// NOTE: The tutorial currently doesn't have an API key here, but it needs one.
+type openWeatherMap struct {
+	apiKey string
+}
+
+func (w openWeatherMap) temperature(city string) (float64, error) {
+	if w.apiKey == "your-key-here" {
+		return 0, errors.New("No API key. Open main.go and set an API key.")
+	}
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=" + w.apiKey + "&q=" + city)
 	if err != nil {
-		return weatherData{}, err
+		return 0, err
 	}
 
 	defer resp.Body.Close()
 
-	var d weatherData
-
-	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
-		return weatherData{}, err
+	var d struct {
+		Main struct {
+			Kelvin float64 `json:"temp"`
+		} `json:"main"`
 	}
 
-	return d, nil
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return 0, err
+	}
+
+	log.Printf("openWeatherMap: %s: %.2f", city, d.Main.Kelvin)
+	return d.Main.Kelvin, nil
+}
+
+// Create Weather Underground-backed weatherProvider implementation
+type weatherUnderground struct {
+	apiKey string
+}
+
+func (w weatherUnderground) temperature(city string) (float64, error) {
+	if w.apiKey == "your-key-here" {
+		return 0, errors.New("No API key. Open main.go and set an API key.")
+	}
+	resp, err := http.Get("http://api.wunderground.com/api/" + w.apiKey + "/conditions/q/" + city + ".json")
+	if err != nil {
+		return 0, err
+	}
+
+	defer resp.Body.Close()
+
+	var d struct {
+		Observation struct {
+			Celsius float64 `json:"temp_c"`
+		} `json:"current_observation"`
+	}
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return 0, err
+	}
+
+	kelvin := d.Observation.Celsius + 273.15
+	log.Printf("weatherUnderground: %s: %.2f", city, kelvin)
+	return kelvin, nil
 }
